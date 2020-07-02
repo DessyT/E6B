1485a53330db84c43086b3bb013b6a6c46c79618
Andrew Trail <andrewtrail@googlemail.com>
2020-07-02 15:55:11 +0100

    Added tests, updated readme

--- tests/tests.py
+++ tests/tests.py
@@ -1,17 +1,8 @@
-# E6B
-Simple python project to perform E6B flight calculations
-
-## Installation
-```bash
-$ pip install e6b
-```
-
-## Usage
-
-### Calculate required flight bearing and final ground speed
-```python
 from e6b import e6b
 
+#Simple unit tests
+
+#Degrees
 windSpeed = 10
 windBearing = 90
 trueAirSpeed = 15
@@ -20,17 +11,11 @@ desiredBearing = 180
 correctedBearing,correction = e6b.getCorrectedBearing(windSpeed,windBearing,trueAirSpeed,desiredBearing)
 groundSpeed = e6b.getGroundSpeed(windSpeed,windBearing,trueAirSpeed,desiredBearing,correction)
 
-print(correctedBearing)
->>  138.18968510422138
-print(groundSpeed)
->>  11.180339887498947
-```
+print("In degrees:")
+print(f"Bearing: {correctedBearing} degrees\nGround Speed: {groundSpeed}")
 
-### Using radians instead of degrees
 
-```python
-from e6b import e6b
-
+#Radians
 windSpeed = 10
 windBearing = 1.5708
 trueAirSpeed = 15
@@ -39,8 +24,5 @@ desiredBearing = 3.14159
 correctedBearing,correction = e6b.getCorrectedBearing(windSpeed,windBearing,trueAirSpeed,desiredBearing,True)
 groundSpeed = e6b.getGroundSpeed(windSpeed,windBearing,trueAirSpeed,desiredBearing,correction,True)
 
-print(correctedBearing)
->>  2.4118623437909346
-print(groundSpeed)
->>  11.180276619728994
-```
+print("\nIn radians:")
+print(f"Bearing: {correctedBearing} degrees\nGround Speed: {groundSpeed}")
